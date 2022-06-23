a86fe0070718f46573eab37d0fdba9cf3de01d37
Levva01 <vellanifederico13@gmail.com>
2022-06-23 16:30:40 +0200

    header top completato

--- index.php
+++ index.php
@@ -5,10 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Google FAQ</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
-    
 
+    <header>
+
+        <div class="header-top">
+            <div class="left-side">
+                <img class="logo" src="img/google-logo.png" alt="Google logo">
+                <div class="privacy-termini">
+                    Privacy e termini
+                </div>
+            </div>
+
+            <div class="right-side">
+                <div class="menu">
+                    &#xe5c3;
+                </div>
+
+                <div class="profile">
+                    <img class="profile-picture" src="img/placeholder.jpg" alt="">
+                </div>
+            </div>
+
+        </div>
+
+        <div class="header-bottom">
+
+        </div>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+    
+    
 <?php
 
     $faqs = [
@@ -102,9 +141,9 @@
     };
     
 
-
-
 ?>
+
+    
 
 
 </body>
